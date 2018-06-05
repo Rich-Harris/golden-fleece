23d34bc29fbba3a470660d48a1d0711b35c575ab
Conduitry <git@fur.army>
2018-06-05 11:05:18 -0400

    support parsing octal integers

--- src/shared.ts
+++ src/shared.ts
@@ -1,7 +1,7 @@
 export const whitespace = /\s/;
 export const validIdentifierCharacters = /[a-zA-Z_$][a-zA-Z0-9_$]*/;
 export const entirelyValidIdentifier = new RegExp('^' + validIdentifierCharacters.source + '$');
-export const number = /^NaN|(?:[-+]?(?:(?:Infinity)|(?:0[xX][a-fA-F0-9]+)|(?:0[bB][01]+)|(?:(?:(?:[1-9]\d*|0)?\.\d+|(?:[1-9]\d*|0)\.\d*|(?:[1-9]\d*|0))(?:[E|e][+|-]?\d+)?)))/
+export const number = /^NaN|(?:[-+]?(?:(?:Infinity)|(?:0[xX][a-fA-F0-9]+)|(?:0[bB][01]+)|(?:0[oO][0-7]+)|(?:(?:(?:[1-9]\d*|0)?\.\d+|(?:[1-9]\d*|0)\.\d*|(?:[1-9]\d*|0))(?:[E|e][+|-]?\d+)?)))/
 
 export const SINGLE_QUOTE = "'";
 export const DOUBLE_QUOTE = '"';
